1d8ca8e0eebff7a0dbc6c381432aadc1b13ce1e7
Jayanth Gowda <jaygowda82@gmail.com>
2025-07-25 10:42:36 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,5 +1,4 @@
-variable "instance_type" {
-  description = "EC2 instance type"
-  type        = string
-  default     = "t2.micro"
+variable "ami_id" {
+  description = "AMI ID for Jenkins EC2 instance"
+  default     = "ami-06a644026f43160a5"
 }
