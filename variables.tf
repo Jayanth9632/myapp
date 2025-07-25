38d06efdc037e3b67587810aad1d2c46d188de9f
Jayanth Gowda <jaygowda82@gmail.com>
2025-07-25 10:03:47 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,19 +1,5 @@
-variable "aws_region" {
-  default = "ap-south-1"
-}
-
 variable "instance_type" {
-  default = "t2.micro"
-}
-
-variable "ami_id" {
-  default = "ami-0f58c7d8c2b2e6f8e"
-}
-
-variable "key_name" {
-  default = "jenkins-key"
-}
-
-variable "public_key_path" {
-  default = "~/.ssh/id_rsa.pub"
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.micro"
 }
